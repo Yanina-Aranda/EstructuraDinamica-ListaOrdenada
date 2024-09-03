caffa9452d004fe07aa07cc01c2f32144c3147a2
Yanina Aranda <yaniiarandaa@gmail.com>
2024-09-03 13:46:41 -0300

    primer commit

---
+++ src/listaordenada/Lista.java
@@ -0,0 +1,82 @@
+package listaordenada;
+import java.io.*;
+
+public class Lista {
+
+    protected Nodo list;
+
+//	************************************* CONSTRUCTOR ******************************************************
+    public Lista() {
+        list = null;
+    }
+
+//	***************************************** INICIO *******************************************************
+    public Nodo inicio() {
+        return list;
+    }
+
+//	***************************************** INICIO *******************************************************
+    public boolean listaVacia() {
+        return (list == null);
+    }
+
+//	********************************** INSERTAR AL PRINCIPIO ***********************************************
+    public void insertarPri(int elem) {
+        Nodo x = new Nodo(elem);
+        if (list == null) {
+            list = x;
+        } else {
+            x.setPs(list);
+            list = x;
+        }
+    }
+
+    public void prt(String s) {
+        System.out.println(s);
+    }
+
+//	******************************************** QUITAR ****************************************************
+    public Nodo quitar(Nodo p, Nodo ant) {
+        Nodo x = p;
+
+        if (p == list) {
+            list = p.getPs();
+        } else {
+            ant.setPs(p.getPs());
+
+        }
+        return x;
+    }
+
+    public Nodo eliminar(int elem) {
+        Nodo x = null;
+        Nodo p = list;
+        Nodo ant = null;
+        if(list == null)
+            return x;
+        boolean band = false;
+        while (p != null && !band) {
+            if (p.getDato() == elem) {
+                band = true;
+            } else {
+                ant = p;
+                p = p.getPs();
+
+            }
+        }
+        if (band == true) {
+            quitar(p, ant);
+            x = p;
+        }
+        return x;
+    }
+
+    public void visualizar() {
+        Nodo p = inicio();
+        while (p != null) {
+            prt(p.getDato() + "");
+            p = p.getPs();
+        }
+    }
+
+}
